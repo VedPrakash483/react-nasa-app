158d8d7394fa2b8d3d33413120bc135ef974eb08
VedPrakash483 <ved.prakash2021@vitstudent.ac.in>
2024-10-27 18:06:50 +0530

    Added SideBar

---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main () {
+    return (
+      <div>
+        <img src="public\mars.png" alt="mars image" />
+      </div>
+    )
+  }
+  
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+      <div>
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+            <p>Description</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex hic minima tempore, voluptates sint perspiciatis quasi quos molestiae eum voluptatem excepturi saepe, porro dolores labore. Dignissimos ducimus modi nesciunt vero!Lorem</p>
+        </div>
+      </div>
+    )
+  }
